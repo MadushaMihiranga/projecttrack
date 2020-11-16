91f1098e2f595c7693900ba69fa11e141bc81e8d
Madusha Mihiranga <madushamihiranga@outlook.com>
2020-11-16 12:09:30 +0530

    Starting the project

---
+++ src/main/java/com/projecttrack/ProjecttrackApplication.java
@@ -0,0 +1,13 @@
+package com.projecttrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projecttrack/ProjecttrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecttrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecttrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
